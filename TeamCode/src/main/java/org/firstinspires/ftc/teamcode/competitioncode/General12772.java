6e68ae1b5143d81c20f303b0230b9c57057adb50
praisethegoldenhoodie <praisethegoldenhoodie@gmail.com>
2018-01-13 10:24:16 -0700

    Introduced general robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competitioncode/General12772.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.competitioncode;
+
+/**
+ * Created by Jose on 1/13/2018.
+ */
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import java.util.Arrays;
+
+class General12772 {
+}
