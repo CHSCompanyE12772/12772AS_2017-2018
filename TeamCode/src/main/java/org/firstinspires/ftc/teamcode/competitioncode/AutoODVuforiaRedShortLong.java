e3271317ee84261bbfd82ca9dd06a09e114436f2
Carlos Barker <carlosbarker@outlook.com>
2018-02-15 20:40:08 -0700

    Merged dev into master. Disregard previous push/commit message.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competitioncode/AutoODVuforiaRedShortLong.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competitioncode/AutoODVuforiaRedShortLong.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode.competitioncode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-
-
 @Autonomous(name = "AutoOD Vuforia Red Short-Long", group = "OD_VF")
 //@Disabled                            //Enables or disables such OpMode (hide or show on Driver Station OpMode List)
 public class AutoODVuforiaRedShortLong extends AutoODVuforia
